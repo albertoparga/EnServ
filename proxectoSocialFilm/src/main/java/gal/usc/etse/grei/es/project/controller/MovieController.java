d0c9e0586abae94910a5197332f1356e7201200a
albertoparga <72313285+albertoparga@users.noreply.github.com>
2023-10-08 11:58:07 +0200

    proyecto base

---
+++ proxectoSocialFilm/src/main/java/gal/usc/etse/grei/es/project/controller/MovieController.java
@@ -0,0 +1,54 @@
+package gal.usc.etse.grei.es.project.controller;
+
+import gal.usc.etse.grei.es.project.model.Movie;
+import gal.usc.etse.grei.es.project.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("movies")
+public class MovieController {
+    private final MovieService movies;
+
+    @Autowired
+    public MovieController(MovieService movies) {
+        this.movies = movies;
+    }
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<Page<Movie>> get(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "20") int size,
+            @RequestParam(name = "sort", defaultValue = "") List<String> sort
+    ) {
+        List<Sort.Order> criteria = sort.stream().map(string -> {
+                    if (string.startsWith("+")) {
+                        return Sort.Order.asc(string.substring(1));
+                    } else if (string.startsWith("-")) {
+                        return Sort.Order.desc(string.substring(1));
+                    } else return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.of(movies.get(page, size, Sort.by(criteria)));
+    }
+
+    @GetMapping(
+            path = "{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<Movie> get(@PathVariable("id") String id) {
+        return ResponseEntity.of(movies.get(id));
+    }
+}
