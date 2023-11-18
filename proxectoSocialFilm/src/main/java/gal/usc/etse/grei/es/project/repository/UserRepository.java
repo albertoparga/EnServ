3b5ba7c88fe64323bf5cc515b55a67595ef97543
Alberto <albertogf2002@gmail.com>
2023-11-18 09:51:02 +0100

    Config hateoas

--- proxectoSocialFilm/src/main/java/gal/usc/etse/grei/es/project/repository/UserRepository.java
+++ proxectoSocialFilm/src/main/java/gal/usc/etse/grei/es/project/repository/UserRepository.java
@@ -2,7 +2,7 @@ package gal.usc.etse.grei.es.project.repository;
 
 import gal.usc.etse.grei.es.project.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {}
